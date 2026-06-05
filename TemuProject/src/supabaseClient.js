433b2767e88f8f371faf61bea248aa403cc85623
MADLANCE45 <marcocretu0@gmail.com>
2026-06-05 08:59:49 +0200

    test3

--- TemuProject/src/supabaseClient.js
+++ TemuProject/src/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Inserisci qui le tue chiavi prese da Supabase (racchiuse tra virgolette)
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// Creiamo l'istanza di connessione esportandola per poterla usare in tutta l'app
 export const supabase = createClient(supabaseUrl, supabaseKey)
